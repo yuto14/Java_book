877f0d83dc9f0d971afedfc7de96d7aec728ea26
yuto14 <programming1410@gmail.com>
2020-08-26 17:30:48 +0900

    [finish]1_1_2

---
+++ Chapter1_1_2.java
@@ -0,0 +1,20 @@
+public class Chapter1_1_2 {
+    public static void main(String[] args) {
+        System.out.println("RPG:スッキリ魔王征伐");
+        System.out.println("Ver.0.1 by 湊");
+        System.out.println("プログラムを終了します。");
+    }
+}
+
+// {}部分をブロック
+// 外側はクラスブロック
+// 内側はメソッドブロック（主に記述する部分）
+
+// クラス名のルール
+// ソースファイル名はクラス名.javaで作成
+// クラス名はアルファベット大文字で始める
+
+
+
+
+
