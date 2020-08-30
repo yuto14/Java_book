db95a9c0369bf9bb3a853983a8b1a64a7eb97ee9
yuto14 <programming1410@gmail.com>
2020-08-30 13:56:40 +0900

    [finish]1_2_1

---
+++ Chapter1_2_1.java
@@ -0,0 +1,11 @@
+public class Chapter1_2_1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = b + 10;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
+// 上記のa,b,5をオペランド、+,=を演算子を呼ぶ。
+//　複雑そうに見える式でも全てはこの２つから構成される。
